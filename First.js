048abafc7ffbbfa1524b08b5041c7cbf201b08ff
Mahad444 <mahadsaid95@icloud.com>
2022-07-19 15:20:46 +0300

    my first javascript

---
+++ First.js
@@ -0,0 +1,128 @@
+// INTRODUCTION
+
+// VARIABLES
+
+// alert("I am learning Javascript");
+/*console.log("Hello world");
+var a=5;
+var b=8;
+var sum=a+b;
+console.log("the answer is "+ sum);
+var firtname='Ally';
+var lastname="Said";
+var myfullname=firtname+" "+lastname;
+console.log("My Full Name is "+myfullname);
+var num=2+'4';
+var num2="456";
+Number(num2);
+console.log(num2);
+
+CONDITIONAL STATEMENTS
+
+var age= parseInt(prompt("please enter your age:"));
+age +=1;
+alert("you are:"+age+"years old");
+var age=parseInt(prompt("please enter your age:"));
+if(age >=18 && age<=120)
+{
+    alert("You can proceed and vote");
+}
+else if(age>=1 && age<=17){
+    alert("you're too young go home");
+}
+ else{
+    alert("invalid input");}*/
+    
+/*var marks=parseInt(prompt("please enter your CAT marks"));
+if (marks>=1 && marks<=30)
+var cat=parseInt();
+else{
+    alert("invalid input");
+}
+
+var marks=parseInt(prompt("please enter your Final Exam marks"));
+ if (marks>=1 && marks<=70);
+ 
+ else{
+    alert("invalid input"); 
+ }*/
+
+// LOOPS
+
+ /*
+ var i=1;
+ while(i<=5){
+    console.log("welcome to class" +i);
+    i++;
+ }
+ var a=1;
+ 
+    do {
+        console.log("welcome to class"+a)
+         a++;
+    }
+    while(a<=5);
+
+for (var i=3; i<=8;i++)
+{
+    console.log("Welcome to Class"+i)
+}
+ 
+for(var a=10; a>=1; a--){
+    console.log("go to class"+a)
+}
+var num=parseInt(prompt("please enter a positive interger: "));
+for (var a=num;a>=0; a--){
+    if(a%2==0){
+        var h6=document.createElement("h6");
+        h6.textContent=a;
+        document.write(a +"<br>")
+    }
+}*/
+
+// FUNCTIONS
+function addNumbers(){
+
+}
+addNumbers();
+function addName(){
+    var num1=10;
+    var num3=5;
+    console.log(num1+num3);
+}
+addName();
+
+function addNam(num1,num3){
+    console.log(num1+num3);
+}
+addNam(10,15);
+
+
+var answer=addNum(25,30);
+console.log(answer);
+function addNum(num1,num2){
+    var answer=num1+num2;
+    return answer;
+
+}
+var age= parseInt(prompt("please choose your optin :\n 1:sh25=250mb for 1hr \n 2:sh20=250mb for 4hr \n"))
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+ 
+    
+
+    
+
