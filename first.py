a1222fc8c07168ea891e9adf509ec83580d643a9
Mahad444 <mahadsaid95@icloud.com>
2022-07-19 15:31:23 +0300

    initial

---
+++ first.py
@@ -0,0 +1,13 @@
+class Contact:
+    '''
+class that generates contact
+'''
+pass 
+
+def __init__(self,name,second,number,email):
+    self.name = name
+    self.second = second 
+    self.number = number 
+    self.email = email 
+
+    
---
+++ switch.js
@@ -0,0 +1,72 @@
+/*var x =1 ;
+ switch(x){
+    case 1:
+    console.log("x is 1");
+    break;
+    
+ 
+    case 2:
+    console.log("x is 2");
+    break;
+
+    case 3:
+    console.log("x is 3");
+   break;
+
+   default:
+    console.log("x is something else");
+
+ }
+ console.log("end of code");*/
+ /*var before=(prompt("Dial *544#"));
+ if (before=="*544#"){
+     prompt("0:NyakuaBonus:\n 1:My Data Bundles\n2:Daily Bundles NEW\n3.Weekly bundle\n4.Monthly Bundles NEW\n5.No Expiry Bundle");
+    var Nyakua=0;
+     if(Nyakua===0){prompt("Nyakua Bonus\n1.Buy Nyakua Bonus\n2.Check My Target\n3.Check My Daily Usage\n4.Check My FREE Data Balance\n5.Join data bonus");
+     }}
+     var before=(prompt("Dial *544#"));
+     switch(before){
+        case "*544#": prompt("0:NyakuaBonus:\n 1.My Data Bundles\n2.Daily Bundles NEW\n3.Weekly bundle\n4.Monthly Bundles NEW\n5.No Expiry Bundle");
+         break;        
+        case'0':
+        var NyakuaBonus= prompt("Nyakua Bonus\n1.Buy Nyakua Bonus\n2.Check My Target\n3.Check My Daily Usage\n4.Check My FREE Data Balance\n5.Join data bus");
+        break;
+        case '1':prompt("My Data Bundle\n1.sh20=250mb for 1hour\n2.sh50=1GB for 2hours\n3.sh1000=15GB for 30days\n4.sh1500=25GB for 30days")
+        break;
+     }*/
+   
+   
+     var before=(prompt("Dial *544#"));
+     if (before=="*544#")
+     var userinput = prompt("please choose any options \n0:Nyakua Bonus\n1:My Data Deals\n2:Daily Bundles\n3:Weekly Bundles\n4:Monthly Bundles\n5:No Expiry\n6:Entertainment Bundles\n7:Okoa Data\n8:Lipa Mdogo Mdogo\n98:MORE");switch(userinput){
+      case '0':
+         var NyakuaBonus=prompt("please choose any options\n1:Buy Nyakua Bundles\n2:Check my Target\n3:check my daily Usage\n4:check my daily usage\n5:join Data Bonus\n6:Opt Out\n7:how it works");
+         break;
+      case '1':
+         var myDataDeals=prompt("please choose any options\n1:sh.20=250mb for 1hour\n2:sh.50=1GB for 2hours\n3:sh.60=2.5GB for untill 5pm")
+         break;
+      case '2':
+         var dailyBundle=prompt("please choose any options\n1:Buy for my number\n2:Buy for other number\n3:Stop Autonrenew Usage")
+         break;
+      case '3':
+         var weeklyBundle=prompt("1:Buy for My Number\n2:Buy for other number\n3:sh.70=300mbs 7days")
+         break;
+      case '4':
+         var monthlyBundle=prompt("please choose any options\n1:Data Only Plans\n2:All in one\n3:Buy for another number\n4:Join Postpay\n5:Easy-Talk minutes\n6:Postpay FAQs")
+         break;
+      case '5':
+         var noExpiry=prompt("please choose any options\n1:Buy for my number\n2:Buy for other number\n3:Okoa Data\n4:Stop autorenewal\n5:Stop autofill")
+         break;
+      case '6':
+         var NyakuaBonus=prompt("please choose any options\n1:Youtube\n2:Showmax\n3:Base Video\n4:Base Music\n5:Angaza\n6:K24 Live")
+         break;
+      case '7':
+         var NyakuaBonus=prompt("Bundles Valid for 24hours\n1:okoa 270mb for 24hrs\n2:okoa 90mb for sh.50\n3:okoa 30mb for sh.10\n okoa 10mb for sh.10")
+         break;
+      case '8':
+         var NyakuaBonus=prompt("Want to Join Lipa mdogo mdogo?Great!Review the terms\n @www.safaricom.co.ke\n1:Accept\n2:Reject\n3:Pay for another")
+}
+function allmarks(){
+   
+}
+    
